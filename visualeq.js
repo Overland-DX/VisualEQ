d3186d2eefc3d7205f876ea0156066d978cd3349
Jan Ove Overland <102610897+Overland-DX@users.noreply.github.com>
2025-08-25 01:21:39 +0200

    Update visualeq.js V1.1

--- visualeq.js
+++ visualeq.js
@@ -1,14 +1,12 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
-    name: 'Visual EQ ALFA',
-    version: '1.0',
-    author: 'Jan Ove Øveland',
+    name: 'VisualEQ',
+    version: '1.1',
+    author: 'ODX',
     frontEndPath: 'visualEQ/visualEQ_main.js'
 }
 
-// Backend (server) changes can go here...
-
-// Don't change anything below here if you are making your own plugin
 module.exports = {
     pluginConfig
+
 }
